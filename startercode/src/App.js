e132d5f50c6cab02077203a7155270fb6e3c044e
Adithi Raghavan <adithiraghavan@Adithis-MacBook-Pro.local>
2021-03-10 12:58:52 -0800

    test final code

--- startercode/src/App.js
+++ startercode/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AirportInfo from './components/AirportInfo';
 
 function App() {
   return (
     <div className="App">
-      Starter Code
+      <Header title="Budget Bookings"></Header>
+      <AirportInfo></AirportInfo>
+      <Footer title="Travel for less with Budget Bookings!"></Footer>
     </div>
   );
 }
--- startercode/src/components/AirportInfo.js
+++ startercode/src/components/AirportInfo.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import './AirportInfo.css';
+import Places from './Places';
 
 function AirportInfo() { 
-    return;
+    const [places,setPlaces] = useState([])
+    const [query,setQuery] = useState("")
+    const [showPlaces,setShowPlaces] = useState(false)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        async function fetchMyAPI() {
+            const reqOptions = {
+                method: 'GET',
+                headers: {
+                    "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`, //Need to Create ENV FiLE
+                    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+                    "useQueryString": true
+                }
+            }
+            let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?" + new URLSearchParams({query: query}), reqOptions)//let = local var
+            response = await response.json()
+            console.log(response.Places)
+            setPlaces(response.Places)
+        }
+        fetchMyAPI()
+        setShowPlaces(true)
+        setQuery("")
+    }
+    return(
+        <div className="airportinfo">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="queryInput">State or Country:</label>
+                <input id="queryInput" value={query} onChange={e => setQuery(e.target.value)} required/>
+                <button className="search">Submit</button>
+            </form>
+            { showPlaces ? <Places places={places}}></Places> : <></>}
+        </div>
+    )
 }
 
 export default AirportInfo;
